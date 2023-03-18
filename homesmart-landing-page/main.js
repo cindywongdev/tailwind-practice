d0f0c3f3b329d55d31a312a80f30d3a9d1a50f95
Cindy Wong <cindywongdev@gmail.com>
2023-03-18 16:16:10 -0400

    added click listener

--- homesmart-landing-page/main.js
+++ homesmart-landing-page/main.js
@@ -1,1 +1,15 @@
 import './style.css'
+
+const navBtn = document.querySelector('#menu')
+const menuBar = document.querySelector('[role="menubar"]')
+
+navBtn.addEventListener('click', () => {
+    // get value of the aria-expanded attribute
+    // isExpanded will either be true or false
+    const isExpanded = JSON.parse(navBtn.getAttribute('aria-expanded'))
+    // flip the value on click
+    navBtn.setAttribute('aria-expanded', !isExpanded)
+    // toggle on click
+    menuBar.classList.toggle('hidden')
+    menuBar.classList.toggle('flex')
+})
